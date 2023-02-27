6a678e20358b268e76367990d8df846f36217ef8
Recio Martínez-Charco <anrh@gft.com>
2023-02-27 21:26:24 +0100

    sorting

---
+++ sorting/Distinct.java
@@ -0,0 +1,30 @@
+
+
+import java.util.ArrayList;
+
+import java.util.List;
+
+public class Distinct {
+    
+
+
+    public int getCountOfDistinctNumbers(int[]givenArr){
+            
+            List <Integer>numsRepOnce=new ArrayList<Integer>(0);
+            for (int i = 0; i < givenArr.length; i++) {
+              if( checkIfInList(givenArr[i], numsRepOnce)==false)numsRepOnce.add(givenArr[i]);
+              
+            }
+            
+            
+            return numsRepOnce.size();
+    
+        }
+        
+        public Boolean checkIfInList(int num,List<Integer> numsRepOnce){
+
+          return numsRepOnce.stream().anyMatch(x->x==num);
+         
+
+        }
+}
