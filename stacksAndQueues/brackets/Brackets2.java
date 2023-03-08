ed01fae6aa26717de25cf32d19a45be593e0cd52
Recio Martínez-Charco <anrh@gft.com>
2023-03-08 18:30:04 +0100

    62% passes all tests, lack of performance

---
+++ stacksAndQueues/brackets/Brackets2.java
@@ -0,0 +1,28 @@
+package com.codilities.stacksAndQueues;
+
+import java.util.HashMap;
+
+public class Brackets2 {
+    public int isItProperlynested(String bracketsStr){
+        if (bracketsStr.length() % 2 != 0)
+        return 0;
+           
+            while (bracketsStr.isEmpty()==false ) {
+            if(bracketsStr.contains("()"))bracketsStr=bracketsStr.replace("()", "");
+            if(bracketsStr.contains("[]"))bracketsStr=bracketsStr.replace("[]", "");
+            if(bracketsStr.contains("{}"))bracketsStr=bracketsStr.replace("{}", ""); 
+            else if(bracketsStr.contains("()"))bracketsStr=bracketsStr.replace("()", "");
+            else if(bracketsStr.contains("[]"))bracketsStr=bracketsStr.replace("[]", "");
+            else if(bracketsStr.contains("{}"))bracketsStr=bracketsStr.replace("{}", ""); 
+               else break;
+            }
+            
+          
+             if (bracketsStr.isEmpty())
+                            return 1;
+
+        return 0;
+    }
+
+
+}
