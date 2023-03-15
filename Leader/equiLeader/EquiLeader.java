bad7f9315e6df2506809a5daa8ec92ebc7a09b62
Recio Martínez-Charco <anrh@gft.com>
2023-03-15 19:08:00 +0100

    equileader passes all correctness tests

--- Leader/equiLeader/EquiLeader.java
+++ Leader/equiLeader/EquiLeader.java
@@ -3,31 +3,53 @@ package com.codilities.leader;
 import java.util.Arrays;
 
 public class EquiLeader {
-
-    public int[] equiLeaderArr;
+      public int biggestCountNum;
+      public int[] equiLeaderArr;
 
     public int amountOfLeaders(int []givenArr) {
        equiLeaderArr = givenArr; 
         int leaderCount = 0;
         if(indxOfNumberWhichCountBiggerThanHalfOfArrayLength()==-1 || equiLeaderArr.length==1)return 0;
-        int biggestCountNum = equiLeaderArr[indxOfNumberWhichCountBiggerThanHalfOfArrayLength()];
-        int comparingNum = 0;
-        int activeLeader=0;
+        biggestCountNum = equiLeaderArr[indxOfNumberWhichCountBiggerThanHalfOfArrayLength()];
         
         for (int i = 0; i < equiLeaderArr.length; i++) {
-            comparingNum = equiLeaderArr[i];
-            if (comparingNum != biggestCountNum) {
-                activeLeader=0;
+           
+            if ( i!=equiLeaderArr.length-1) {
+                if(isLeaderInFirstHalf(i)==true && isLeaderInSecondHalf(i)==true){
+                    leaderCount++;
+                }
             }
-            if (comparingNum == biggestCountNum && activeLeader!=1) {
-                leaderCount++;
-                activeLeader = 1;
-            }
+            
 
         }
         return leaderCount;
 
     }
+
+     public Boolean isLeaderInFirstHalf(int indx){
+            int firstSeqncLength=equiLeaderArr.length-(equiLeaderArr.length-1-indx);
+            int firstSeqncHalfLength=Math.floorDiv(firstSeqncLength,2);
+            int possibleLeaderCount=0;
+            for (int i = 0; i < firstSeqncLength; i++) {
+                int  comparingNum = equiLeaderArr[i];
+            if (comparingNum==biggestCountNum)possibleLeaderCount++;
+            if(possibleLeaderCount>firstSeqncHalfLength)return true;
+            }
+            
+            return false;
+     }
+     public Boolean isLeaderInSecondHalf(int indx){
+        int secondSeqncLength=equiLeaderArr.length-indx-1;
+        int secondSeqncHalfLength=Math.floorDiv(secondSeqncLength,2);
+            int possibleLeaderCount=0;
+            for (int i = indx+1; i < equiLeaderArr.length; i++) {
+                int  comparingNum = equiLeaderArr[i];
+            if (comparingNum==biggestCountNum)possibleLeaderCount++;
+            if(possibleLeaderCount>secondSeqncHalfLength)return true;
+            }
+            
+            return false;
+     }
 
     public int indxOfNumberWhichCountBiggerThanHalfOfArrayLength() {
         
--- Leader/equiLeader/EquiLeaderTest.java
+++ Leader/equiLeader/EquiLeaderTest.java
@@ -2,6 +2,7 @@ package com.codilities;
 
 import static org.junit.Assert.assertEquals;
 
+
 import org.junit.Test;
 
 
@@ -47,4 +48,55 @@ public class EquiLeaderTest {
     assertEquals(2, leaderCount);  
 
  }
+ @Test
+ public void amountOfLeadersWhenArrayGotOneNum(){
+    int[]givenArray=new int[]{4};
+    int leaderCount=equiLeader.amountOfLeaders(givenArray);
+    assertEquals(0, leaderCount);  
+
+ }
+ @Test
+ public void amountOfLeadersWhenArrayGotTwoEqualNumbers(){
+    int[]givenArray=new int[]{2,2};
+    int leaderCount=equiLeader.amountOfLeaders(givenArray);
+    assertEquals(1, leaderCount);  
+
+ }
+ @Test
+ public void givenAnIndexProvesThatInFirstSequenceTheresALeader(){
+   EquiLeader equiLeader=new EquiLeader();
+      equiLeader.equiLeaderArr=new int[]{2,2};
+      equiLeader.biggestCountNum=2;
+    boolean isALeader=equiLeader.isLeaderInFirstHalf(0);
+    assertEquals(true, isALeader);  
+
+ }
+ @Test
+ public void givenAnIndexProvesThatInSecondSequenceTheresALeader(){
+   EquiLeader equiLeader=new EquiLeader();
+      equiLeader.equiLeaderArr=new int[]{2,2};
+      equiLeader.biggestCountNum=2;
+    boolean isALeader=equiLeader.isLeaderInSecondHalf(0);
+    assertEquals(true, isALeader);  
+
+ }
+ @Test
+ public void givenAnIndexProvesThatInFirstSequenceTheresNotALeader(){
+   EquiLeader equiLeader=new EquiLeader();
+      equiLeader.equiLeaderArr=new int[]{3,1,2,2};
+      equiLeader.biggestCountNum=2;
+    boolean isALeader=equiLeader.isLeaderInFirstHalf(1);
+    assertEquals(false, isALeader);  
+
+ }
+ @Test
+ public void givenAnIndexProvesThatInSecondSequenceTheresNotALeader(){
+   EquiLeader equiLeader=new EquiLeader();
+      equiLeader.equiLeaderArr=new int[]{4,3,1,2,2};
+      equiLeader.biggestCountNum=2;
+    boolean isALeader=equiLeader.isLeaderInSecondHalf(0);
+    assertEquals(false, isALeader);  
+
+ }
+
 }
